9b732e9cdf1b98c67aef4a1f20ce2a0493e7c4d9
Tomas Krizek <tomas.krizek@nic.cz>
2021-05-10 15:38:18 +0200

    prepare: wait for systemd startup

--- prepare.sh
+++ prepare.sh
@@ -28,6 +28,9 @@ start_container() {
 
     echo "Waiting up to 60s for working DNS resolution..."
     lxc-attach -n "$CONTAINER_ID" -- /bin/bash -c "for i in {1..60}; do if getent hosts $SERVER_HOST &>/dev/null; then exit 0; fi; sleep 1; done; exit 1"
+
+    echo "Waiting up to 30s for systemd startup..."
+    lxc-attach -n "$CONTAINER_ID" -- /bin/bash -c "for i in {1..30}; do if systemctl is-system-running -q &>/dev/null; then exit 0; fi; sleep 1; done; exit 1"
 }
 
 echo "Running in $CONTAINER_ID"
